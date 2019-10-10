1cb406fbe8c69ce92d7980274c8ca83379ab3a02
一叶知秋 <34859156+VJaGG@users.noreply.github.com>
2019-10-10 19:50:57 +0800

    Update 3.2.1、reverse.py

--- chapter2/3.2.1、reverse.py
+++ chapter2/3.2.1、reverse.py
@@ -3,7 +3,7 @@ def reverse(x):
     x = 255-x
     return x
 if __name__ == '__main__':
-    image = cv2.imread('Fig0304(a)(breast_digital_Xray).tif',cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread('./image/Fig0304(a)(breast_digital_Xray).tif',cv2.IMREAD_GRAYSCALE)
     print(image.shape)
     cv2.imshow('original',image)
     image = reverse(image)
